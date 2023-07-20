766545a29a10cf0c0b73fab6e7a9ffa203a93203
Somraj <somraj21mondal@gmail.com>
2023-07-20 11:20:28 +0530

    Updates

---
+++ precache-manifest.2d17bf1306c5c7ec17e7503454e249db.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1f7697db3dd8356328f",
+    "url": "/somrajportfolioreacttest/static/css/main.7048acb8.chunk.css"
+  },
+  {
+    "revision": "a1f7697db3dd8356328f",
+    "url": "/somrajportfolioreacttest/static/js/main.a1f7697d.chunk.js"
+  },
+  {
+    "revision": "f6c9ecc587c59d5eb52b",
+    "url": "/somrajportfolioreacttest/static/css/1.df066e28.chunk.css"
+  },
+  {
+    "revision": "f6c9ecc587c59d5eb52b",
+    "url": "/somrajportfolioreacttest/static/js/1.f6c9ecc5.chunk.js"
+  },
+  {
+    "revision": "b5b85ccbd3d2c1ae545a",
+    "url": "/somrajportfolioreacttest/static/js/runtime~main.b5b85ccb.js"
+  },
+  {
+    "revision": "26ced5ba6db75431c8c5749ac43ac3d3",
+    "url": "/somrajportfolioreacttest/static/media/homeimage.26ced5ba.jpg"
+  },
+  {
+    "revision": "ec660a068a6b6a6a8bd19c52876b838e",
+    "url": "/somrajportfolioreacttest/static/media/somraj.ec660a06.jpeg"
+  },
+  {
+    "revision": "ff8d650569eb0dd46a5d02553d3f22e7",
+    "url": "/somrajportfolioreacttest/static/media/Content Writing.ff8d6505.jpg"
+  },
+  {
+    "revision": "0d5c89c437430471c345575bac8de0cb",
+    "url": "/somrajportfolioreacttest/static/media/Web Development.0d5c89c4.jpg"
+  },
+  {
+    "revision": "e1efac732d1a8fe112dacaa77c8ea5c7",
+    "url": "/somrajportfolioreacttest/static/media/aboutpage.e1efac73.jpg"
+  },
+  {
+    "revision": "f594ede5fc4d573f9a0d5b4c49fb8fa1",
+    "url": "/somrajportfolioreacttest/static/media/Python Development.f594ede5.jpg"
+  },
+  {
+    "revision": "9b683e0f5a2acaa690bca159a1619e8f",
+    "url": "/somrajportfolioreacttest/static/media/Blogging.9b683e0f.jpg"
+  },
+  {
+    "revision": "290d56d89d9c5d95344d69e96be61733",
+    "url": "/somrajportfolioreacttest/static/media/seo.290d56d8.jpg"
+  },
+  {
+    "revision": "b8f0994c00e807a197319ef21d531cf9",
+    "url": "/somrajportfolioreacttest/static/media/WordPress Site.b8f0994c.jpg"
+  },
+  {
+    "revision": "a58f009c137894796e1521b227a4e159",
+    "url": "/somrajportfolioreacttest/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/somrajportfolioreacttest/precache-manifest.2d17bf1306c5c7ec17e7503454e249db.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/somrajportfolioreacttest/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
